f564b7fad201325049ba8a807c62367201aeb13b
Jeremie Dimino <jdimino@janestreet.com>
2016-07-13 10:46:31 +0100

    114.01+24

--- src/command.ml
+++ src/command.ml
@@ -3,6 +3,18 @@ open Import
 
 include Core.Std.Command
 
+type 'a with_options = ?extract_exn:bool -> 'a
+
+let shutdown_with_error e =
+  prerr_endline (Error.to_string_hum e);
+  shutdown 1
+;;
+
+let maybe_print_error_and_shutdown = function
+  | Ok () -> shutdown 0
+  | Error e -> shutdown_with_error e
+;;
+
 (* For command line applications, we want the following behavior: [async-command-exe |
    less] should never truncate the output of the exe.
 
@@ -26,51 +38,79 @@ include Core.Std.Command
    contain almost nothing, or even be /dev/null), so we make them all the time.
 *)
 let in_async ?extract_exn param on_result =
-  Param.map param ~f:(fun main () ->
-    let before_shutdown () =
-      Deferred.List.iter ~how:`Parallel
-        Writer.[ force stdout; force stderr ]
-        ~f:(fun writer ->
-          Deferred.any_unit
-            [ Writer.close_finished writer
-            ; Writer.consumer_left writer
-            ; Writer.flushed writer
-            ])
-    in
-    Shutdown.at_shutdown before_shutdown;
-    Shutdown.set_default_force
-      (let prev = Shutdown.default_force () in
-       fun () ->
-         Deferred.all_unit
-           (* The 1s gives a bit of time to the process to stop silently rather than with
-              a "shutdown forced" message and exit 1 if [prev ()] finished first. *)
-           [ prev (); (before_shutdown () >>= fun () -> after (sec 1.)) ]);
-    upon (Deferred.Or_error.try_with ?extract_exn main) on_result;
-    (never_returns (Scheduler.go ()) : unit)
+  Param.map param ~f:(fun staged_main () ->
+    let main = Or_error.try_with (fun () -> unstage (staged_main ())) in
+    match main with
+    | Error e -> shutdown_with_error e
+    | Ok main ->
+      let before_shutdown () =
+        Deferred.List.iter ~how:`Parallel
+          Writer.[ force stdout; force stderr ]
+          ~f:(fun writer ->
+            Deferred.any_unit
+              [ Writer.close_finished writer
+              ; Writer.consumer_left writer
+              ; Writer.flushed writer
+              ])
+      in
+      Shutdown.at_shutdown before_shutdown;
+      Shutdown.set_default_force
+        (let prev = Shutdown.default_force () in
+         fun () ->
+           Deferred.all_unit
+             (* The 1s gives a bit of time to the process to stop silently rather than with
+                a "shutdown forced" message and exit 1 if [prev ()] finished first. *)
+             [ prev (); (before_shutdown () >>= fun () -> after (sec 1.)) ]);
+      upon
+        (Deferred.Or_error.try_with ?extract_exn (fun () -> main `Scheduler_started))
+        on_result;
+      (never_returns (Scheduler.go ()) : unit)
   )
 ;;
 
-let maybe_print_error_and_shutdown = function
-  | Ok () -> shutdown 0
-  | Error e ->
-    prerr_endline (Error.to_string_hum e);
-    shutdown 1
+type 'r staged = ([`Scheduler_started] -> 'r) Staged.t
+
+module Staged = struct
+  let async' ?extract_exn ~summary ?readme param =
+    let on_result = maybe_print_error_and_shutdown in
+    basic' ~summary ?readme (in_async ?extract_exn param on_result)
+  ;;
+
+  let async ?extract_exn ~summary ?readme spec main =
+    async' ?extract_exn ~summary ?readme (Spec.to_param spec main)
+  ;;
+
+  let async_or_error' ?extract_exn ~summary ?readme param =
+    let on_result res = maybe_print_error_and_shutdown (Or_error.join res) in
+    basic' ~summary ?readme (in_async ?extract_exn param on_result)
+  ;;
+
+  let async_or_error ?extract_exn ~summary ?readme spec main =
+    async_or_error' ?extract_exn ~summary ?readme (Spec.to_param spec main)
+  ;;
+end
+
+let stage_param =
+  Param.map ~f:(fun main ->
+    fun () ->
+      stage (fun `Scheduler_started -> main ()))
 ;;
 
-let async' ?extract_exn ~summary ?readme param =
-  let on_result = maybe_print_error_and_shutdown in
-  basic' ~summary ?readme (in_async ?extract_exn param on_result)
+let async'      ?extract_exn ~summary ?readme param =
+  Staged.async' ?extract_exn ~summary ?readme (stage_param param)
+;;
+
+let async_or_error'      ?extract_exn ~summary ?readme param =
+  Staged.async_or_error' ?extract_exn ~summary ?readme (stage_param param)
 ;;
 
 let async ?extract_exn ~summary ?readme spec main =
-  async' ?extract_exn ~summary ?readme (Spec.to_param spec main)
-
-let async_or_error' ?extract_exn ~summary ?readme param =
-  let on_result res = maybe_print_error_and_shutdown (Or_error.join res) in
-  basic' ~summary ?readme (in_async ?extract_exn param on_result)
+  async'  ?extract_exn ~summary ?readme (Spec.to_param spec main)
 ;;
 
 let async_or_error ?extract_exn ~summary ?readme spec main =
-  async_or_error' ?extract_exn ~summary ?readme (Spec.to_param spec main)
+  async_or_error'  ?extract_exn ~summary ?readme (Spec.to_param spec main)
+;;
 
 let async_basic = async
+
--- src/command.mli
+++ src/command.mli
@@ -1,33 +1,64 @@
 (** [Async.Command] is [Core.Command] with additional Async functions. *)
 
+open! Core.Std
 open! Import
 
 include module type of Core.Std.Command
   with type t      = Core.Std.Command.t
   with module Spec = Core.Std.Command.Spec
 
+type 'a with_options
+  =  ?extract_exn : bool
+  -> 'a
+
 (** [async] is like [Core.Command.basic], except that the main function it expects returns
     [unit Deferred.t], instead of [unit].  [async] will also start the Async scheduler
     before main is run, and will stop the scheduler when main returns.
 
-    [async] also handles top-level exceptions by wrapping the user-supplied function in
-    a [Monitor.try_with]. If an exception is raised, it will print it to stderr and call
+    [async] also handles top-level exceptions by wrapping the user-supplied function in a
+    [Monitor.try_with]. If an exception is raised, it will print it to stderr and call
     [shutdown 1]. The [extract_exn] argument is passed along to [Monitor.try_with]; by
     default it is [false].
 *)
-val async  : ?extract_exn:bool -> ('a, unit Deferred.t) basic_command
-val async' : ?extract_exn:bool -> unit Deferred.t basic_command'
+val async  : ('a, unit Deferred.t) basic_command  with_options
+val async' :      unit Deferred.t  basic_command' with_options
 
 (** [async_basic] is a deprecated synonym for [async] that will eventually go away.  It is
     here to give code outside jane street a chance to switch over before we delete it. *)
-val async_basic : ?extract_exn:bool -> ('a, unit Deferred.t) basic_command
+val async_basic : ('a, unit Deferred.t) basic_command with_options
   [@@deprecated "[since 2015-10] Use async instead"]
 
 (** [async_or_error] is like [async], except that the main function it expects may
     return an error, in which case it prints out the error message and shuts down with
     exit code 1. *)
-val async_or_error  : ?extract_exn:bool -> ('a, unit Deferred.Or_error.t) basic_command
-val async_or_error' : ?extract_exn:bool -> unit Deferred.Or_error.t basic_command'
+val async_or_error  : ('a, unit Deferred.Or_error.t) basic_command  with_options
+val async_or_error' :      unit Deferred.Or_error.t  basic_command' with_options
+
+(* staged functions allow the main function to be separated into two stages.  The first
+   part is guaranteed to run before the async scheduler is started, and the second part
+   will run after the async scheduler is started.  This is useful if the main function
+   runs code that relies on the fact that threads have not been created yet
+   (e.g. [Daemon.daemonize]).
+
+   As an example:
+   {[
+     let main () =
+       assert (not (Scheduler.is_running ()));
+       stage (fun `Scheduler_started ->
+         assert (Scheduler.is_running ());
+         Deferred.unit
+       )
+   ]}
+*)
+
+type 'r staged = ([`Scheduler_started] -> 'r) Staged.t
+
+module Staged : sig
+  val async           : ('a, unit Deferred.t          staged) basic_command  with_options
+  val async'          :      unit Deferred.t          staged  basic_command' with_options
+  val async_or_error  : ('a, unit Deferred.Or_error.t staged) basic_command  with_options
+  val async_or_error' :      unit Deferred.Or_error.t staged  basic_command' with_options
+end
 
 (** To create an [Arg_type.t] that uses auto-completion and uses Async to compute the
     possible completions, one should use
