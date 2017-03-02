fe47bad290f166f15cc864ebd89961eb336f16b0
Jeremie Dimino <jdimino@janestreet.com>
2017-03-02 17:25:32 +0000

    114.34+110

--- src/import.ml
+++ src/import.ml
@@ -1,8 +1,4 @@
-include (Async_unix.Import
-         : (module type of struct include Async_unix.Import end
-             with module Require_explicit_time_source
-             := Async_unix.Import.Require_explicit_time_source))
-
+include Async_unix.Import
 include Async_unix.Std
 
 module Rpc_kernel    = Async_rpc_kernel.Std.Rpc
